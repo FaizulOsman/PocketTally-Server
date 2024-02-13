d719f47fecd25210abc7e1037d50fcaf32bc530f
FaizulOsman <faizulosmanrabby@gmail.com>
2024-02-13 22:59:48 +0600

    site on live now

---
+++ dist/app/modules/user/user.validation.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userValidation = void 0;
+const zod_1 = require("zod");
+const updateUserZodSchema = zod_1.z.object({
+    body: zod_1.z.object({
+        email: zod_1.z.string().optional(),
+        password: zod_1.z.string().optional(),
+        role: zod_1.z.enum(['user', 'admin', 'super_admin']).optional(),
+        name: zod_1.z.string().optional(),
+        phone: zod_1.z.string().optional(),
+        address: zod_1.z.string().optional(),
+        imageUrl: zod_1.z.string().optional(),
+    }),
+});
+exports.userValidation = {
+    updateUserZodSchema,
+};
